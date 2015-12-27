826cf90d85c730ebeda3b11a764744a696610c9c
akan03 <akan1920@gmail.com>
2015-12-27 17:31:14 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -19,6 +19,9 @@ invrt <- NULL
              getInverse = getInverse)
 }
 
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cachesolve should retrieve the inverse from the cache
 
 
 
